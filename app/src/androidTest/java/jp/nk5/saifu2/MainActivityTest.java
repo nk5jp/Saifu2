3af567ca52ab930efd32ebec5fcfaa9be0b3642b
nk5jp <keigo.nagata@gmail.com>
2019-02-24 01:21:21 +0900

    Change MainActivity to DrawerLayout.

---
+++ app/src/androidTest/java/jp/nk5/saifu2/MainActivityTest.java
@@ -0,0 +1,29 @@
+package jp.nk5.saifu2;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void listGoesOverTheWorld() {
+        onView(withText("Hello World!")).check(matches(isDisplayed()));
+    }
+}
--- app/src/main/java/jp/nk5/saifu2/MainActivity.java
+++ app/src/main/java/jp/nk5/saifu2/MainActivity.java
@@ -2,6 +2,8 @@ package jp.nk5.saifu2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView1);
+        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[]{"aaa", "bbb"}));
     }
+
 }
